70314659b4d2d8e5a1d5b3b41ac9e7ea2a462442
Andrey <Foxinthenight@mail.ru>
2024-03-24 14:09:00 +0300

    A list has been created without interactivity

---
+++ app/src/main/java/com/example/pizzastudy/PizzaRecipeAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.pizzastudy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pizzastudy.databinding.ActivityMainBinding
+import com.example.pizzastudy.databinding.PizzaRecipeItemBinding
+
+class PizzaRecipeAdapter(val pizzaRecipeItems : ArrayList<PizzaRecipeltem>) : RecyclerView.Adapter<PizzaRecipeAdapter.PizzaHolder>(){
+    class PizzaHolder(item : View) : RecyclerView.ViewHolder(item){
+        private val binding = PizzaRecipeItemBinding.bind(item)
+        fun bind(itemPizza: PizzaRecipeltem) = with(binding){
+            pizzaImageView.setImageResource(itemPizza.imageResource)
+            titleImageView.text = itemPizza.title
+            descriptionImageView.text = itemPizza.description
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PizzaHolder {
+        val view:View = LayoutInflater.from(parent.context).inflate(R.layout.pizza_recipe_item,parent,false)
+        return PizzaHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return pizzaRecipeItems.size
+    }
+
+    override fun onBindViewHolder(holder: PizzaHolder, position: Int) {
+       val item = pizzaRecipeItems.get(position)
+        holder.bind(pizzaRecipeItems[position])
+    }
+
+}
---
+++ app/src/main/java/com/example/pizzastudy/PizzaRecipeltem.kt
@@ -0,0 +1,4 @@
+package com.example.pizzastudy
+
+data class PizzaRecipeltem(val imageResource : Int, val title : String, val description : String ) {
+}
