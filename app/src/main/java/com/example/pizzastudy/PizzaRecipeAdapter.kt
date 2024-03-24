9d6d56d54fde532d54b6e9fd9861167e332af528
Andrey <Foxinthenight@mail.ru>
2024-03-24 18:44:19 +0300

    Added item selection and opening of a new activity

--- app/src/main/java/com/example/pizzastudy/PizzaRecipeAdapter.kt
+++ app/src/main/java/com/example/pizzastudy/PizzaRecipeAdapter.kt
@@ -1,20 +1,36 @@
 package com.example.pizzastudy
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pizzastudy.databinding.PizzaRecipeItemBinding
 
-class PizzaRecipeAdapter(private val pizzaRecipeItems : ArrayList<PizzaRecipeltem>) : RecyclerView.Adapter<PizzaRecipeAdapter.PizzaHolder>(){
-    class PizzaHolder(item : View) : RecyclerView.ViewHolder(item){
+class PizzaRecipeAdapter(private val pizzaRecipeItems : ArrayList<PizzaRecipeltem> , val context: Context) : RecyclerView.Adapter<PizzaRecipeAdapter.PizzaHolder>(){
+    inner class PizzaHolder(item : View ) : RecyclerView.ViewHolder(item), OnClickListener {
         private val binding = PizzaRecipeItemBinding.bind(item)
+        init {
+            item.setOnClickListener(this)
+        }
         fun bind(itemPizza: PizzaRecipeltem) = with(binding){
             pizzaImageView.setImageResource(itemPizza.imageResource)
             titleImageView.text = itemPizza.title
             descriptionImageView.text = itemPizza.description
         }
 
+        override fun onClick(v: View? ) {
+
+            val position = adapterPosition
+            val intent = Intent( context , RecipeActivity::class.java)
+            val pizzaRecipeltem = pizzaRecipeItems[position]
+            intent.putExtra("title", pizzaRecipeltem.title)
+            intent.putExtra("descr", pizzaRecipeltem.description)
+
+            context.startActivity(intent)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PizzaHolder {
--- app/src/main/java/com/example/pizzastudy/RecipeActivity.kt
+++ app/src/main/java/com/example/pizzastudy/RecipeActivity.kt
@@ -2,10 +2,17 @@ package com.example.pizzastudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizzastudy.databinding.ActivityRecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityRecipeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe)
+        setContentView(binding.root)
+        val title = binding.titleTextView
+        val descipt = binding.descriptionTextView
+        val intent = intent
+        intent?.getStringExtra("title").also {title.text = it }
+        intent?.getStringExtra("descr").also {descipt.text = it }
     }
 }
